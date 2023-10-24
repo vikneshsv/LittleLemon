bb7e7aa0b2e810ffcaec2868ac4703dce208b8b0
vikneshsv <vikneshvicky2669@gmai.com>
2023-10-24 18:37:26 +0530

    nav is modified

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,49 @@
+import React, { useState } from "react";
+import logo from "../images/Logo.svg";
 
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-const Nav=()=>{
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <a href="/" className="logo">
+        <img src={logo} alt="" />
+      </a>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
 
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/booking">Order Online</a>
+        </li>
+        <li>
+          <a href="/" id="login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
+
 export default Nav;
